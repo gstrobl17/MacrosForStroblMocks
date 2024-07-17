d21f53db857102b89ff989ed1e1faeceb8cf72bc
Greg Strobl <gstrobl17@gmail.com>
2024-07-16 22:15:13 -0400

    Removed use of SwiftLintPlugins

--- Package.swift
+++ Package.swift
@@ -20,7 +20,6 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
-        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", .upToNextMajor(from: "0.55.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -37,15 +36,13 @@ let package = Package(
         // Library that exposes a macro as part of its API, which is used in client programs.
         .target(
             name: "MacrosForStroblMocks",
-            dependencies: ["MacrosForStroblMocksMacros"],
-            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]
+            dependencies: ["MacrosForStroblMocksMacros"]
         ),
 
         // A client of the library, which is able to use the macro in its own code.
         .executableTarget(
             name: "MacrosForStroblMocksClient",
-            dependencies: ["MacrosForStroblMocks"],
-            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]
+            dependencies: ["MacrosForStroblMocks"]
         ),
 
         // A test target used to develop the macro implementation.
